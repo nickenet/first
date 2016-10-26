c37a25f1bdcd543e98bcdf89d535e534997f0514
Nicke <nicke.ipxp@gmail.com>
2016-10-26 20:19:26 +0300

    start page of site and nb to gitignore list

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Yo-ho-ho</h1>
+        <img style="width:100%" src="http://www.extreme-factory.ru/wp-content/uploads/2011/12/Santa-Klausyi.jpg"/>
+    </body>
+</html>
